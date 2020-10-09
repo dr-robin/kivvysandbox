a2751e4c7deee6cf353ac0039ec8030b2a776557
becode <robinmcgregor@gmail.com>
2020-10-09 21:09:16 +0200

    added buildozer.spec

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import kivy
 kivy.require('1.11.1')  # Set to your Kivy version
 from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 
 class MyApp(App):
     def build(self):
-        return Button(text='Learn')
+        return Builder.load_file('main.kv')
 
 
 MyApp().run()
